3979e5901808497de7035fee92de9fd123ff6e27
a-mar92 <57841204+a-mar92@users.noreply.github.com>
2022-05-26 08:55:28 +0200

    Add files via upload

---
+++ OperatorTernarny.java
@@ -0,0 +1,9 @@
+public class OperatorTernarny {
+    public static void main(String[] args) {
+        boolean isRaining;
+        String message;
+        isRaining = false;
+        message = isRaining? "Take umbrella" : "sunny " ;
+        System.out.println(message);
+    }
+}
