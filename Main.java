a1921c29a2982616d57dad7c0678e3ab73da4f56
a-mar92 <57841204+a-mar92@users.noreply.github.com>
2022-06-02 10:55:19 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,28 @@
-import java.net.Proxy;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int startValue;
-        double numberOfMonths;
-
-        double procent;
-        double finalValue;
-
-        System.out.println("Deposit your Money :" );
-        startValue = Integer.valueOf(scanner.nextInt());
-        System.out.println("How long you give me money ? in month :");
-        numberOfMonths = Integer.valueOf(scanner.nextInt());
-        //numberOfMonths = Math.floor(numberOfMonths/12);
-        System.out.println("how many procent ? ");
-        procent= scanner.nextDouble();
-        finalValue =startValue *Math.pow((1+ (procent/100)),(Math.floor(numberOfMonths/12)));
-
-        int year = (int) Math.floor(numberOfMonths/12);
-
-        System.out.println(finalValue);
-        System.out.println("atert " + year + "years  yours interest is  " + (finalValue - startValue));
+        URL url;
+        try {
+            url = new URL("https://api.chucknorris.io/jokes/random");
+            InputStream name = url.openStream();
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(name)
+            );
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
 
 
-    }
-}
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }}
